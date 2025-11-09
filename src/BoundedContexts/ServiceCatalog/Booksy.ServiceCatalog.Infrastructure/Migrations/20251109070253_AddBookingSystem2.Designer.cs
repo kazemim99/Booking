375ae161cd81a4625a4eef5e4e19eb711deccb55
mo.kazemi <mo.kazemi@exino.org>
2025-11-09 10:49:22 +0330

    add migration

--- src/BoundedContexts/ServiceCatalog/Booksy.ServiceCatalog.Infrastructure/Migrations/20251109070253_AddBookingSystem2.Designer.cs
+++ src/BoundedContexts/ServiceCatalog/Booksy.ServiceCatalog.Infrastructure/Migrations/20251109070253_AddBookingSystem2.Designer.cs
@@ -4,6 +4,7 @@ using Booksy.ServiceCatalog.Infrastructure.Persistence.Context;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -11,9 +12,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace Booksy.ServiceCatalog.Infrastructure.Migrations
 {
     [DbContext(typeof(ServiceCatalogDbContext))]
-    partial class ServiceCatalogDbContextModelSnapshot : ModelSnapshot
+    [Migration("20251109070253_AddBookingSystem2")]
+    partial class AddBookingSystem2
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
